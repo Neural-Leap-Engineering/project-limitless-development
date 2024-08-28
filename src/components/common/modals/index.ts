f727b8bf9e39d59f46ce4a8df604504ad28ebb72
Neural-Leap-Engineering <60061519+Neural-Leap-Engineering@users.noreply.github.com>
2024-08-28 12:49:53 +0530

    Delete src/components directory

--- src/components/common/modals/index.ts
+++
@@ -1,9 +0,0 @@
-import ModalEditEvent from "./modal-edit-event/ModalEditEvent";
-import ModalCreateEvent from "./modal-create-event/ModalCreateEvent";
-import ModalDayInfo from "./modal-day-info/ModalDayInfo";
-
-export {
-  ModalEditEvent,
-  ModalCreateEvent,
-  ModalDayInfo
-}
