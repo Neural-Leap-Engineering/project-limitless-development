da92f798116c287ec57591fb75f4357edd2bdae8
Neural-Leap-Engineering <60061519+Neural-Leap-Engineering@users.noreply.github.com>
2024-08-28 12:50:33 +0530

    Delete src/store directory

--- src/store/rootActions.ts
+++
@@ -1,9 +0,0 @@
-import * as eventsActions from './events/actions';
-import * as modalsActions from './modals/actions';
-import * as popusActions from './popups/actions';
-
-export const allActions = {
-  ...eventsActions,
-  ...modalsActions,
-  ...popusActions
-}
--- src/store/rootReducer.ts
+++
@@ -1,9 +0,0 @@
-import { reducer as eventsReducer } from './events/reducer';
-import { reducer as modalsReducer } from './modals/reducer';
-import { reducer as popupsReducer } from './popups/reducer';
-
-export const reducers = {
-  events: eventsReducer,
-  modals: modalsReducer,
-  popups: popupsReducer
-}
