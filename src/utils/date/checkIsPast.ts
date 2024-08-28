d6576bfe9e16f91d94f70f3a1699225b35eb6548
Neural-Leap-Engineering <60061519+Neural-Leap-Engineering@users.noreply.github.com>
2024-08-28 12:50:52 +0530

    Delete src/utils directory

--- src/utils/date/checkIsPast.ts
+++
@@ -1,7 +0,0 @@
-import { checkIsToday } from './checkIsToday';
-
-export const checkIsPast = (date: Date) => {
-  const today = new Date();
-
-  return (today.getTime() > date.getTime()) && !checkIsToday(date);
-};
