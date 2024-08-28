91f1cd6ff81369484bc63a83cbf700082ed813ad
Neural-Leap-Engineering <60061519+Neural-Leap-Engineering@users.noreply.github.com>
2024-08-28 12:52:42 +0530

    Delete babel.config.js

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: '> 1%', useBuiltIns: 'usage', corejs: 3 }],
-    '@babel/preset-typescript',
-    '@babel/preset-react',
-  ],
-  plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-transform-runtime'],
-};
